684eab6f93f7c5b306f8c846a162acfe84b4284d
mahdieh1992 <mohamadimahdieh70@gmail.com>
2025-09-23 11:10:00 +0330

    exercise65

---
+++ Exercise65.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def day_calculator(dateStr):
+    mainDate = "1999-01-14"
+    mainDate = datetime.strptime(mainDate,'%Y-%m-%d').date()
+    dateStr = datetime.strptime(dateStr,'%Y-%m-%d').date()
+    if dateStr < mainDate:
+        result = "Not yet born"
+    else:
+        result = (dateStr - mainDate).days
+    return result
+    
+print(day_calculator('1118-10-13'))
