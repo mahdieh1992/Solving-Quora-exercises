dae4aed97a96d29342d6f5371b123f882c1e4b6b
mahdieh1992 <mohamadimahdieh70@gmail.com>
2025-10-08 10:30:13 +0330

    Periodic exercises 4

--- Exercise90.py
+++ Exercise90.py
@@ -2,7 +2,7 @@
 def good_fruits(fruits: tuple[dict]) -> dict[str,int]:
     fruits_special = {}
     for d_fruit in fruits:
-            if d_fruit['shape'] == 'sphere' and (d_fruit['mass'] >= 300 and d_fruit['mass'] <= 600) and (d_fruit['volume'] >= 100 and d_fruit['volume'] <= 500):
+            if d_fruit['shape'] == 'sphere' and (300 <= d_fruit['mass'] <= 600) and (100 <= d_fruit['volume'] <= 500):
                 fruits_special[d_fruit['name']] = fruits_special.get(d_fruit['name'],0) + 1
     return fruits_special
 
